731db060be245da7c3d878c68e8aa127425495ec
nageshwar-fortmindz <nageshwar.mondal2020@gmail.com>
2024-05-09 20:33:51 +0530

    test12

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,15 @@ const MyApp = ({ Component, pageProps }) => {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
